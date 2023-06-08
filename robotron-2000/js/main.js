32af40e06bb7a416dc749ee88bed76d7c024ca45
ElielRegiani <elielregiani@gmail.com>
2023-06-08 12:49:33 -0300

    Adding Statistics Controls

--- robotron-2000/js/main.js
+++ robotron-2000/js/main.js
@@ -1,6 +1,8 @@
 const robotron = document.querySelector('#robotron')
 robotron.addEventListener("click", sayHello)
 const control = document.querySelectorAll(".controle-ajuste")
+const estatisticas = document.querySelectorAll("[data-estatistica]")
+console.log(estatisticas)
 
 function sayHello() {
     console.log("Hello!")
@@ -8,15 +10,56 @@ function sayHello() {
 
 control.forEach((element) => {
     element.addEventListener("click", (event) => {
-        manageData(event.target.textContent, event.target.parentNode)
+        manageData(event.target.dataset.controle, event.target.parentNode)
+        atualizaEstatisticas(event.target.dataset.peca)
     })
 })
 
-function manageData(operacao, parentNode) {
-    const piece = parentNode.querySelector(".controle-contador")
+function manageData(operacao, peca) {
+    const piece = peca.querySelector("[data-contador]")
     if (operacao === "-") {
         piece.value = parseInt(piece.value) - 1
     } else {
         piece.value = parseInt(piece.value) + 1
+    }
+}
+
+function atualizaEstatisticas(peca) {
+    estatisticas.forEach( (element) => {
+        element.textContent = parseInt(element.textContent) + pecas[peca][element.dataset.estatistica]
+    })
+}
+
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
     }
 }
